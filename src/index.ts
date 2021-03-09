46743b5cb6fbb3fa6b82e1a633a0e8060e01838c
Lucas de Araujo <araujolucas97@outlook.com>
2021-03-08 21:45:07 -0300

    upload photos

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './helpers/env';
 import app from './server';
 import startConnection from './database/index';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 startConnection().then(() => {
   app.listen(PORT, () => {
